52a7ceb65bb16bd0d65cad5a7bf42cbe43745beb
Arnau <arnaucat@gmail.com>
2024-09-28 13:41:50 +0200

    new file api.js

---
+++ js/api.js
@@ -0,0 +1,40 @@
+
+    //Arnau DB
+    const url = "https://projectesalle-default-rtdb.europe-west1.firebasedatabase.app/" 
+
+    const project = "grup-9/"
+    const database = "formulari.json"
+
+    fetch(url + project + database)
+    .then(res => res.json())
+    .then(res => {
+        console.log(res)
+        
+        let idAlumne
+
+        for (const id in res) {
+            if (res[id].nom == "Carlos") {
+                idAlumne = id;
+                break;
+            }
+        }
+
+        console.log("ID de Carlos: " + idAlumne);
+    })
+
+    function postAlumne(nom) {
+        fetch(url + project + database, {
+            method: 'POST',
+            body: JSON.stringify({nom})
+        })
+        .then(res => res.json())
+        .then(res => console.log(res))
+    }
+
+    function obtenerDatos(){
+        fetch(url + project + database)
+        .then(res => res.json())
+        .then(res => {
+            console.log(res)
+            })
+    }
